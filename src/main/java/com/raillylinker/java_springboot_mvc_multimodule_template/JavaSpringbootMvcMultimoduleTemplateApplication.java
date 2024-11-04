320afe6aa3ae41ea2fa3b545cae4fc68c57f2332
RaillyLinker <raillylinker@gmail.com>
2024-11-04 10:16:33 +0900

    feat : Init Project

---
+++ src/main/java/com/raillylinker/java_springboot_mvc_multimodule_template/JavaSpringbootMvcMultimoduleTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.raillylinker.java_springboot_mvc_multimodule_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootMvcMultimoduleTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootMvcMultimoduleTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raillylinker/java_springboot_mvc_multimodule_template/JavaSpringbootMvcMultimoduleTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raillylinker.java_springboot_mvc_multimodule_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootMvcMultimoduleTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
