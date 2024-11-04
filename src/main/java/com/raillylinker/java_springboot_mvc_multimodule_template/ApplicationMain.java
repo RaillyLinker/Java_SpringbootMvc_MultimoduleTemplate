ab4600d17158315f56ccb3644b2d1c38a24d9381
RaillyLinker <raillylinker@gmail.com>
2024-11-04 12:03:18 +0900

    feat : Springboot 멀티 모듈 구조

---
+++ src/main/java/com/raillylinker/java_springboot_mvc_multimodule_template/ApplicationMain.java
@@ -0,0 +1,33 @@
+package com.raillylinker.java_springboot_mvc_multimodule_template;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.TimeZone;
+
+@ComponentScan(
+        basePackages = {
+                // !!!Bean 스캔할 모듈들의 패키지 리스트 추가하기!!!
+                "com.raillylinker.module_l1_api_sample_v1"
+        }
+)
+//@EntityScan("com.raillylinker.module_idp_jpa.jpa_beans.entities")
+//@EnableJpaRepositories("com.raillylinker.module_idp_jpa.jpa_beans.repositories")
+@SpringBootApplication
+public class ApplicationMain {
+    @Bean
+    public CommandLineRunner init() {
+        return args -> {
+            // 서버 타임존 명시적 설정 (UTC, Asia/Seoul, ...)
+            TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+//            System.out.println("Current TimeZone: " + TimeZone.getDefault().getID());
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationMain.class, args);
+    }
+}
