59b9fb9687b8856ef2fdbe8cc7cf591ba05ae4d9
RaillyLinker <raillylinker@gmail.com>
2024-11-06 10:29:35 +0900

    feat : retrofit2 모듈 추가

--- module-app/src/main/java/com/raillylinker/module_app/ApplicationMain.java
+++ module-app/src/main/java/com/raillylinker/module_app/ApplicationMain.java
@@ -19,15 +19,11 @@ import java.util.TimeZone;
         basePackages = {
                 // !!!Bean 스캔할 모듈들의 패키지 리스트 추가하기!!!
                 "com.raillylinker.module_app",
-
                 "com.raillylinker.module_scheduler",
-
                 "com.raillylinker.module_socket",
-
                 "com.raillylinker.module_kafka",
-
                 "com.raillylinker.module_redis",
-
+                "com.raillylinker.module_retrofit2",
                 "com.raillylinker.module_api_project"
         }
 )
